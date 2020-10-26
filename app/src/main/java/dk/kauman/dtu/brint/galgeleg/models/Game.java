fa7f58568714328ad815bd5b43fa12ba76588ba8
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-10-26 11:25:00 +0100

    Implement game status

--- app/src/main/java/dk/kauman/dtu/brint/galgeleg/models/Game.java
+++ app/src/main/java/dk/kauman/dtu/brint/galgeleg/models/Game.java
@@ -1,10 +1,25 @@
 package dk.kauman.dtu.brint.galgeleg.models;
 
+import dk.kauman.dtu.brint.galgeleg.models.enums.GameStatus;
+
 public class Game {
 
     private final String word;
+    private GameStatus status = GameStatus.IN_PROGRESS;
 
     public Game(String word) {
         this.word = word;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public boolean isWon() {
+        return this.status.equals(GameStatus.WON);
+    }
+
+    public boolean isLost() {
+        return this.status.equals(GameStatus.LOST);
     }
 }
---
+++ app/src/main/java/dk/kauman/dtu/brint/galgeleg/models/enums/GameStatus.java
@@ -0,0 +1,5 @@
+package dk.kauman.dtu.brint.galgeleg.models.enums;
+
+public enum GameStatus {
+    IN_PROGRESS, WON, LOST;
+}
